d5f970bd1512b02f95c2176f0c4d58929f0aa3ed
januch <andersen.jn@gmail.com>
2021-01-12 12:09:04 +0100

    endpoint to generate a signature

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.8
 
 COPY ./app /app
+COPY requirements.txt .
+RUN pip install -r ./requirements.txt
--- app/main.py
+++ app/main.py
@@ -1,9 +1,33 @@
 from flask import Flask
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from base64 import b64encode
+import time
 app = Flask(__name__)
+key = RSA.generate(1024)
 
 @app.route("/")
 def hello():
-	return "Hello World from Flask"
+	current_time = time.strftime("%H:%M:%S")
+	return "Hello World from Flask - %s" % current_time
+
+@app.route("/sign/<message>")
+def sign(message):
+	digest = SHA256.new()
+	digest.update(message.encode('utf-8'))
+	signer = PKCS1_v1_5.new(key)
+	sig = signer.sign(digest)
+	encoded = b64encode(sig)
+	return "Signature %s" % encoded
+
+@app.route("/hi/<username>")
+def hi(username):
+	return "Hi {}".format(escape(username))
+
+@app.route("/foo")
+def foo():
+	return "FOO!"
 
 if __name__ == "__main__":
 	# Only for debugging while developing
